f800252789e2baf26947bade88f4b1a7a0c945a3
Emogy-reunion <mv7786986@gmail.com>
2024-04-14 22:56:29 -0700

    first commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+#!/usr/bin/env puppet
+# Ensure specific version of Flask (2.1.0) is installed
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
